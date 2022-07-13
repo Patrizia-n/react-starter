aee3b1ba5e5560a123aca1ca41718109e71b5bbe
Chris <fablekey@gmail.com>
2022-07-13 13:54:19 +1000

    started work on Main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function MainPage() {
+  return (
+    <div id="main-body">
+      <h1>
+        Platypod is the best!
+      </h1>
+      <p>This is just a little test React App</p>
+    </div>
+  )
+};
