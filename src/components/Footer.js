64c090b50056e5c1b73c1e5bd5dcaf043fe8bb0d
patrizia-n <pt.nittoli@gmail.com>
2022-07-13 16:38:42 +1000

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+        <div>
+            <h4>Copyright <span><img src="./copyrights-icon.png" alt="copyrights icon" width="25px" /></span>  jwd8 Generation Australia </h4>
+        </div>
+    )
+}
